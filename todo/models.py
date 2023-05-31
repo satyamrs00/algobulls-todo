d4bc1f85652af70c8fac8ba6ec59e56b17b78878
Satyam Sagar <satyamsagar1711@gmail.com>
2023-05-31 05:30:21 +0530

    api CRUD working

---
+++ todo/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+STATUS_CHOICES = [ (0, 'OPEN'), (1, 'WORKING'), (2, 'DONE'), (3, 'OVERDUE') ]
+class User(AbstractUser):
+    pass
+
+class Todo(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    due_date = models.DateTimeField(blank=True, null=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
